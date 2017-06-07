a612b202a4533ddb973dd12ba4d549b2d242ed41
anuroop <anuroop.buggaveeti15@gmail.com>
2017-06-07 11:40:16 -0700

    RFA forms 01

--- app/javascript/common/inputFields.js
+++ app/javascript/common/inputFields.js
@@ -1,1 +1,11 @@
 import React from 'react'
+
+
+const InputComponent = ({gridClassName, labelClassName, id, label, onChange, value, placeholder, type}) => (
+  <div className={gridClassName}>
+    <label className={labelClassName} htmlFor={id}>{label}</label>
+    <input id={id} type={type} placeholder={placeholder} value={value} onChange={onChange}/>
+  </div>
+)
+
+export {InputComponent}
