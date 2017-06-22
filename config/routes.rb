4aa95ad4fdc72997d41d8c888bc7246c7c79d0d5
Lavanya Reddy <lreddy@cambriasolutions.com>
2017-06-21 21:02:49 -0700

    add dictionary actions in rfa controller and get dictionaries from api

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,16 @@ Rails.application.routes.draw do
   # heartbeat page
   get 'heartbeat', to: 'heartbeat#show'
 
-  get 'rfa', to: 'rfa#show'
+  resource :rfa, controller: :rfa, only: [] do
+    member do
+      get :show
+      get :index
+      get :residence_ownership_types
+      get :languages
+      get :name_types
+      get :phone_number_types
 
+      post :residence
+    end
+  end
 end
