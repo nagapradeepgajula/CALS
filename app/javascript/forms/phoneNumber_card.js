23fb479ef18c0ef1a10da49dddded34d4c6d6c7a
anuroop <anuroop.buggaveeti15@gmail.com>
2017-06-22 11:03:31 -0700

    separated Name and Phone cards

---
+++ app/javascript/forms/phoneNumber_card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {PhoneNumberField} from '../common/phoneNumberField'
+import {InputComponent} from '../common/inputFields';
+import {DropDownField} from '../common/dropDownField';
+
+export default class PhoneComponent extends React.Component {
+	render () {
+		return (
+			<div className="col-md-12">
+        <PhoneNumberField  />
+      </div>
+		)
+	}
+}
